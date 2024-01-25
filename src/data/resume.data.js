1a02535f8d97f93a8a5abbe6e3ee9ac3957e31bf
Natalia Ribeiro <natilgr@hotmail.com>
2024-01-25 18:23:21 -0300

    delete: unused lines in resume.data.js

--- src/data/resume.data.js
+++ src/data/resume.data.js
@@ -3,35 +3,30 @@ const resumeData = [{
     {
       title: "Desenvolvimento Web: Python",
       date: "Em curso",
-      // description: "Parte da formação do Curso de desenvolvimento Web Full Stack da Trybe.",
       badges: [
         'Python', 'TDD', 'POO', 'Flask', 'Django']
     },
     {
       title: "Floripa + Tec",
       date: "Em curso",
-      // description: "Parte da formação do Curso de desenvolvimento Web Full Stack da Trybe.",
       badges: [
         'JS', 'HTML', 'CSS', 'React', 'PostgreSQL', 'Express']
     },
     {
       title: "Módulo desenvolvimento Web: Front-end",
       date: "2023",
-      // description: "Parte da formação do Curso de desenvolvimento Web Full Stack da Trybe.",
       badges: [
         'Html', 'Css', 'Javascript', 'React', 'Redux', 'Context Api', 'Jest', 'RTL']
     },
     {
       title: "Módulo Desenvolvimento Web: Back-end",
       date: "2023",
-      // description: "Parte da formação do Curso de desenvolvimento Web Full Stack da Trybe.",
       badges: [
         'JS', 'Node', 'Express', 'MySQL', 'ORM', 'Docker', 'JWT', 'Chai', 'Mocha', 'Sinon', 'SOLID', 'POO', 'Arquitetura MSC']
     },
     {
       title: " Módulo Desenvolvimento Web: Ciência da Computação",
       date: "2023",
-      // description: "Parte da formação do Curso de desenvolvimento Web Full Stack da Trybe.",
       badges: [
         'Python', 'Estrutura de dados', 'Algorítmos', 'Paradigmas de programação',]
     },
@@ -45,7 +40,7 @@ const resumeData = [{
   ],
   proBackground: [
     {
-      year: "2023 - até o momento",
+      year: "2023-2024",
       role: "Transição de carreira",
       company_name: "",
       job_description: "Iniciei meus estudos para transição de carreira, através do curso intensivo de Desenvolvimento Web FullStack da Trybe.",
@@ -54,7 +49,7 @@ const resumeData = [{
       year: "2020-2022",
       role: "Sócia Proprietária",
       company_name: "Yalla Laboratório Culinário",
-      job_description: "Fui responsável pela gestão de pessoas, compras e produção dos produtos vendidos. Neste período empregamos 3 pessoas e construímos uma presença digital com mais de <span>2k seguidores</span> em nossas redes sociais.",
+      job_description: "Fui responsável pela gestão de pessoas, compras e produção dos produtos vendidos. Neste período empregamos 3 pessoas e construímos uma presença digital com mais de 2k seguidore em nossas redes sociais.",
     },
     {
       year: "2016-2019",
